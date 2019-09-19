d6dbf2191953af46c90aa1f0e58a5ccffbeb386a
김영재 <byeongal@kookmin.ac.kr>
2019-09-19 15:08:26 +0900

    Week02

---
+++ week02/code04.py
@@ -0,0 +1,5 @@
+int_value = 23
+float_value = 3.141592
+
+print(type(int_value))
+print(type(float_value))
---
+++ week02/code05.py
@@ -0,0 +1,10 @@
+a = 1
+b = 3
+
+print(a, "+", b, "=", a+b)
+print(a, "-", b, "=", a-b)
+print(a, "*", b, "=", a*b)
+print(a, "/", b, "=", a/b)
+print(a, "//", b, "=", a//b)
+print(a, "%", b, "=", a%b)
+print(a, "**", b, "=", a**b)
