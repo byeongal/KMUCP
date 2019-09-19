422f96b29c060cfd90e0f87069d51fa3d80e780d
김영재 <byeongal@kookmin.ac.kr>
2019-09-19 15:54:35 +0900

    Week03

---
+++ week03/code07.py
@@ -0,0 +1,8 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+print(alphabet[3:])
+print(alphabet[:10])
+print(alphabet[::2])
+print(alphabet[3:10])
+print(alphabet[3:10:2])
+
---
+++ week03/code11.py
@@ -0,0 +1,5 @@
+pokemon = ["피카츄", "라이츄", "파이리", "꼬부기", "버터플", "야도란", "피죤투", "또가스"]
+
+print(pokemon[:3])
+print(pokemon[3:])
+print(pokemon[::2])
