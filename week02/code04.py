eae7313d90f712515b863f9cc402fb597eb00319
김영재 <byeongal@kookmin.ac.kr>
2019-09-19 15:13:56 +0900

    Week02

--- week02/code04.py
+++ week02/code04.py
@@ -2,4 +2,4 @@ int_value = 23
 float_value = 3.141592
 
 print(int_value, "는", type(int_value), "입니다.")
-print(type(float_value), "는", type(int_value), "입니다.")
+print(float_value, "는", type(int_value), "입니다.")
