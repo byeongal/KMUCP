072c079c2ad51e52971c72343d35298f567567a9
김영재 <byeongal@kookmin.ac.kr>
2019-09-19 15:07:52 +0900

    Week02

---
+++ week02/code06.py
@@ -0,0 +1,8 @@
+print(int('3'))
+print(int(3.14))
+print(float('3.14'))
+print(float('3'))
+print(float(3))
+# print(int('안녕하세요'))
+# print(int('3.14'))
+# print(float('안녕하세요'))
---
+++ week02/code07.py
@@ -0,0 +1,5 @@
+int_value = int(input("정수를 입력해 주세요. >"))
+float_value = float(input("실수를 입력해 주세요 >"))
+
+print("입력하신 정수는", int_value, "입니다.")
+print("입력하신 실수는", float_value, "입니다.")
