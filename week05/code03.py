281e06359f52ba81e7c15e96174c5fd63f185b30
김영재 <corea_kyj@naver.com>
2019-09-26 14:45:15 +0900

    4주차 업데이트

---
+++ week05/code03.py
@@ -0,0 +1,6 @@
+message = "안녕하세요. 저는 김영재입니다."
+print("한줄에 출력하기")
+print(message)
+print("한글자씩 출력하기")
+for character in message:
+    print(character)
---
+++ week05/code04.py
@@ -0,0 +1,12 @@
+print("0부터 9까지 출력")
+for i in range(10):
+    print(i)
+print("3부터 9까지 출력")
+for i in range(3, 9):
+    print(i)
+print("2부터 10까지 짝수 출력")
+for i in range(2, 11, 2):
+    print(i)
+print("10부터 1까지 출력")
+for i in range(10, 0, -1):
+    print(i)
