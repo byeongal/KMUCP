08fb36c73b2b95d428c6466ec49cb76dc39c6607
김영재 <byeongal@kookmin.ac.kr>
2019-09-28 13:55:46 +0900

    문제1번 #입력, #출력, #정수, #연산

--- week02/practice.py
+++
@@ -1,6 +0,0 @@
-weight = float(input("몸무게(kg)을 입력해 주세요 >> "))
-height = float(input("키(m)를 입력해 주세요 >> "))
-
-bmi = weight / (height * height)
-
-print("당신의 BMI지수는", bmi, "입니다.")
--- week03/practice.py
+++
@@ -1,6 +0,0 @@
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-
-print(int(a+b) + int(c+d))
