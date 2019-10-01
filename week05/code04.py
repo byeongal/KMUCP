d80c40e152adc2ca8cec024683f002ff4614e750
김영재 <byeongal@kookmin.ac.kr>
2019-10-01 20:54:44 +0900

    5주차 :: 9->10 변경

--- week05/code04.py
+++ week05/code04.py
@@ -2,7 +2,7 @@ print("0부터 9까지 출력")
 for i in range(10):
     print(i)
 print("3부터 9까지 출력")
-for i in range(3, 9):
+for i in range(3, 10):
     print(i)
 print("2부터 10까지 짝수 출력")
 for i in range(2, 11, 2):
